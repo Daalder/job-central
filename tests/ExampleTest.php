0fc4a4ab488689e566e9a0558e0f6b9d89c34309
Vincent van Hoven <Gazotey@users.noreply.github.com>
2021-03-16 16:28:12 +0100

    [ADD] Github workflow

---
+++ tests/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Daalder\SkeletonPackage\Tests;
+
+/**
+ * Class ExampleTest
+ * @package Daalder\SkeletonPackage\Tests
+ */
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
