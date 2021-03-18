687203a00041fca87d57b1711baa9be972b270fe
Vincent van Hoven <Vincentvanhoven@outlook.com>
2021-03-18 16:58:00 +0100

    [TEST] Github Action failure

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -58,6 +58,10 @@ class TestCase extends DaalderTestCase
             }
         }
 
+        $this->callNonExistingFunction();
+        sdfsdfsdf;
+        die();
+
         $orchestra = __DIR__ . '/../vendor/orchestra/testbench-core/laravel';
         $migrationDirectory = realpath(__DIR__ . '/../vendor/pionect/daalder/database/migrations');
         $migrations = array_diff(scandir($migrationDirectory), ['..', '.']);
