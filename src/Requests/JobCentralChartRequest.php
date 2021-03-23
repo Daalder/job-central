c42a4631622d24ce08725ea789d70c682d6c9a84
Vincent van Hoven <Vincentvanhoven@outlook.com>
2021-03-23 17:32:59 +0100

    [IMPROVE] Request validation

--- src/Requests/JobCentralChartRequest.php
+++ src/Requests/JobCentralChartRequest.php
@@ -23,11 +23,14 @@ class JobCentralChartRequest extends Request
             $jobs[$key] = Arr::last(explode('\\', $job));
         }
 
+        $maxDays = config('job-central.keep-logs-for-days');
+        $maxHours = $maxDays * 24;
+
         return [
             'job' => ['required_without:group', 'empty_with:group', 'string', Rule::in($jobs)],
             'group' => ['required_without:job', 'empty_with:job', 'string', Rule::in($groups)],
-            'days' => ['required_without:hours', 'empty_with:hours', 'numeric', 'integer'],
-            'hours' => ['required_without:days', 'empty_with:days', 'numeric', 'integer'],
+            'days' => ['required_without:hours', 'empty_with:hours', 'numeric', 'integer', 'min:1', 'max:'.$maxDays],
+            'hours' => ['required_without:days', 'empty_with:days', 'numeric', 'integer', 'min:1', 'max:'.$maxHours],
         ];
     }
 }
